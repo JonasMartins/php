917ebcaaca3561a1cda1a695ddc3197c75542ecd
JonasMartins <jonas.s.martins@hotmail.com>
2016-08-24 22:33:40 -0300

    Retornado dados

--- Essential_Training/CH15_MySQL/databases.php
+++ Essential_Training/CH15_MySQL/databases.php
@@ -14,6 +14,20 @@
 		mysqli_error() . "(" . mysqli_errno() .")" );
 	}
 ?>
+<?php
+	// perform database query
+	$query = "SELECT * ";
+	$query .= "FROM subjects ";
+	$query .= "WHERE visible = 1 ";
+	$query .= "ORDER BY position DESC";
+
+	$result = mysqli_query($connection,$query);
+	// result is a resource type, a new one
+	if(!$result){
+		die("database connection faild.");
+	} 
+
+?>
 <!DOCTYPE html PUBLIC "~//W3C//DTD HTML 4.0.1 Transitional//EN"
 	"http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
@@ -21,7 +35,19 @@
 	<title>Databases</title>
 	</head>
 	<body>
-		
+
+		<?php
+			// enquanto puder retornar algum valor...
+			while($row = mysqli_fetch_row($result)){
+				var_dump($row);
+				echo "<hr /> "; 
+			}
+
+		?>
+		<?php
+			// liberando o reesultado
+			mysqli_free_result($result);
+		?>
 	</body>
 </html>
 <?php
