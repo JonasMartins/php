073e2e607b53f91379a8efa712a67870a239b359
JonasMartins <jonas.s.martins@hotmail.com>
2016-08-27 19:41:02 -0300

    Assets/Ativos reusáveis

---
+++ widget_corp/includes/layouts/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+	<title>Widged Corp</title>
+	<link rel="stylesheet" type="text/css" href="stylesheets/public.css" media="all">
+</head>
+	<body>
+		<div id="header">
+			<h1>Widget Corp</h1>
+		</div>
---
+++ widget_corp/public/manage_content.php
@@ -0,0 +1,15 @@
+<?php	
+/** Header, footer e functions, como aproveita-las
+em outros arquivos  */
+require_once("../includes/functions.php");
+include("../includes/layouts/header.php"); ?>
+
+<div id="main">
+	<div id="navigation">
+		&nbsp;	
+	</div>
+	<div id="page">
+		<h2>Manage Content</h2>
+	</div>
+</div>	
+<?php include("../includes/layouts/footer.php"); ?>
