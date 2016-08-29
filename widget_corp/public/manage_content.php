b85855599580a3564235c3db17cda60be49791ff
JonasMartins <jonas.s.martins@hotmail.com>
2016-08-28 21:51:12 -0300

    Navegação da página manage_content

--- widget_corp/public/manage_content.php
+++ widget_corp/public/manage_content.php
@@ -8,29 +8,6 @@ require_once("../includes/db_connection.php");
 em outros arquivos  */
 require_once("../includes/functions.php"); 
 ?>
-<?php
-
-	// perform database query
-	$query = "SELECT * ";
-	$query .= "FROM subjects ";
-	$query .= "WHERE visible = 1 ";
-	$query .= "ORDER BY position DESC";
-
-
-	$query2 = "SELECT * ";
-	$query2 .= "FROM subjects ";
-	$query2 .= "WHERE visible = 0 ";
-	$query2 .= "ORDER BY position ASC";
-
-
-	$result = mysqli_query($connection,$query);
-	$result2 = mysqli_query($connection,$query2);
-
-	/** Usando uma função de functions.php */
-	confirm_query($result);
-	confirm_query($result);
-	 
-?>
 <?php include("../includes/layouts/header.php"); ?>
 
 <div id="main">
@@ -46,34 +23,50 @@ require_once("../includes/functions.php");
 			/* Fazendo um loop para mostrar todas as informações dos subjects, no caso apenas uma tebela e não um objeto em sí, notar como
 			for feito a quebra no meio do loop para listar
 			de uma forma desejável as informações na página.
-			*/
-			while($subject = mysqli_fetch_assoc($result)){
-				?>
-				<li><?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?></li>
-			<?php
-				}
+			*/	
+			$query = "SELECT * ";
+			$query .= "FROM subjects ";
+			$query .= "WHERE visible = 1 ";
+			$query .= "ORDER BY position ASC";
+			$result = mysqli_query($connection,$query);
+			confirm_query($result);
+		
+	 		while($subject = mysqli_fetch_assoc($result)){
+				/* Abaixo da lista de subjects, queremos mostrar
+				uma lista com todas as páginas
+				*/
+				?>	
+				<li>				
+				<?php echo $subject["menu_name"]; ?>
+					<?php	
+						$query = "SELECT * ";
+						$query .= "FROM pages ";
+						$query .= "WHERE visible = 1 ";
+						$query .= "AND subject_id = {$subject["id"]} "; /* cuidado espaço.... */
+						$query .= "ORDER BY position ASC";
+						$page_set = mysqli_query($connection,$query);
+						confirm_query($page_set);	
+					?>
+					<ul class="pages">
+					<?php
+					while($page = mysqli_fetch_assoc($page_set)){
+						?>	
+						<li><?php echo $page["menu_name"]; ?></li>
+					<?php 
+						}
+					 ?>
+					<?php mysqli_free_result($page_set); ?>
+					</ul>
+				</li>
+			<?php 
+				} 
 			?>
-			</ul>	 
-			<br />
-			<ul class="subjects">
-			<?php
-			/* Tirar as informações invisíveis */
-			echo "Invisibles: <br />";
-			while($subject = mysqli_fetch_assoc($result2)){
-				?>
-				<li><?php echo $subject["menu_name"] . " (" . $subject["id"] . ")"; ?></li>
-			<?php
-				}
-			?>	
-			</ul>
-	</div>
+	<?php mysqli_free_result($result); ?>
+		</ul>
+		</div>
 	<div id="page">
 		<h2>Manage Content</h2>
 	</div>
-</div>	
-<?php
-	// liberando o reesultado
-	mysqli_free_result($result);
-?>
+</div>	 
 <?php include("../includes/layouts/footer.php"); ?>
 <!-- Close connection foi movido para o footer-->
