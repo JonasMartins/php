37e3c385ff526994e3e3ca462c18b58320484334
JonasMartins <jonas.s.martins@hotmail.com>
2016-09-03 20:02:02 -0300

    Refatorando novamente

--- widget_corp/includes/functions.php
+++ widget_corp/includes/functions.php
@@ -34,5 +34,67 @@
 		confirm_query($result); 
 		return $result;
 	}
+	/* Os dois argumentos de navigation são:
+	o atual subject selecionado e a atual page
+	selecionada  */
+	function navigation($subject_id,$page_id){
+	/*	
+	<!-- Esses ids e classes se referem ao css,
+			ou seja os estilo que foi definido para navigation, subjects e todos listados aqui
+			serão aplicados nestes padrões, notar a grande
+			diferença que faz ao se adotar o css como 
+			design da página --> */
+			$output_html = "<ul class=\"subjects\">";
+			$subject_set = find_all_subjects();
+			
+	 		while($subject = mysqli_fetch_assoc($subject_set)){
+				/* Abaixo da lista de subjects, queremos mostrar
+				uma lista com todas as páginas
+				*/	
+				$output_html .= "<li";// concatenando tudo
+				/* artificion feito para musar a classe
+				selected vinda do stylesheet para que 
+				o nome do objeto subject esteja em bold
+				apenas se o mesmo estiver selecionado
+				por isso o if dentro do php, dentro do
+				li */
+				if($subject["id"]==$subject_id){
+					$output_html .=  "class=\"selected\"";
+					/*se os dois forem iguais aadiciona
+					a marcação selected, do stylesheet */
+				}
+				$output_html .=  ">"; 
+				$output_html .= "<a href=\"manage_content.php?subject=";
+				$output_html .=  urlencode($subject["id"]);
+				$output_html .= "\">";
+				$output_html .=  $subject["menu_name"];
+				$output_html .= "</a>";  
 
+				/*	Pages  */
+				$page_set = find_pages_for_subject($subject["id"]);
+				$output_html .= "<ul class=\"pages\">";
+				while($page = mysqli_fetch_assoc($page_set)){  
+					$output_html .=  "<li";
+					if($page["id"]==$page_id){
+						$output_html .=  "class=\"selected\"";
+					}
+					$output_html .=  ">"; 
+					$output_html .= "<a href=\"manage_content.php?page=";
+					$output_html .=  urlencode($page["id"]); 
+					$output_html .= "\">";
+					$output_html .=  $page["menu_name"];
+					$output_html .= "</a>"; 
+					$output_html .= "</li>"; 
+				}
+			mysqli_free_result($page_set);
+			$output_html .= "</ul>";
+			$output_html .= "</li>";
+			} 
+	mysqli_free_result($subject_set); 
+		$output_html .= "</ul>";
+	/* retornando todo o html em uma unica string para
+	a pagina que requisitar essa função, uma forma
+	de refatoração mais uma vez*/	
+	return $output_html;	
+	}
 ?>
--- widget_corp/public/manage_content.php
+++ widget_corp/public/manage_content.php
@@ -24,64 +24,7 @@ require_once("../includes/functions.php");
 ?>
 <div id="main">
 	<div id="navigation">
-			<!-- Esses ids e classes se referem ao css,
-			ou seja os estilo que foi definido para navigation, subjects e todos listados aqui
-			serão aplicados nestes padrões, notar a grande
-			diferença que faz ao se adotar o css como 
-			design da página -->
-			<ul class="subjects">
-			<?php $subject_set = find_all_subjects(); ?>
-			<?php
-	 		while($subject = mysqli_fetch_assoc($subject_set)){
-				/* Abaixo da lista de subjects, queremos mostrar
-				uma lista com todas as páginas
-				*/
-				?>	
-				<?php 
-				echo "<li";
-				/* artificion feito para musar a classe
-				selected vinda do stylesheet para que 
-				o nome do objeto subject esteja em bold
-				apenas se o mesmo estiver selecionado
-				por isso o if dentro do php, dentro do
-				li */
-				if($subject["id"]==$selected_subject_id){
-					echo "class=\"selected\"";
-					/*se os dois forem iguais aadiciona
-					a marcação selected, do stylesheet */
-				}
-				echo ">"; 
-				?>			
-				<a href="manage_content.php?subject=<?php
-				echo urlencode($subject["id"]); ?>"><?php echo $subject["menu_name"]; ?></a>  
-					<?php	
-							$page_set = find_pages_for_subject($subject["id"]);
-					?>
-					<ul class="pages">
-					<?php
-					while($page = mysqli_fetch_assoc($page_set)){
-						?>	
-							<?php 
-							echo "<li";
-							if($page["id"]==$selected_page_id){
-								echo "class=\"selected\"";
-							}
-							echo ">"; 
-						?>
-						<a href="manage_content.php?page=<?php
-				echo urlencode($page["id"]); ?>"><?php echo $page["menu_name"]; ?></a> 
-						</li>
-					<?php 
-						}
-					 ?>
-					<?php mysqli_free_result($page_set); ?>
-					</ul>
-				</li>
-			<?php 
-				} 
-			?>
-	<?php mysqli_free_result($subject_set); ?>
-		</ul>
+		<?php echo navigation($selected_subject_id,$selected_page_id); ?>
 		</div>
 	<div id="page">
 		<h2>Manage Content</h2>
