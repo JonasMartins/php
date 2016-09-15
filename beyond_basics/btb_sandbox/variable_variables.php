ebd251918203f34bda8abab206b0f864399a5b39
JonasMartins <jonas.s.martins@hotmail.com>
2016-09-14 23:00:53 -0300

    Begin POO beyond the basics

---
+++ beyond_basics/btb_sandbox/variable_variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Variable Variables</title>
+</head>
+<body>
+<?php
+	
+	/*
+	$a = "Hello";
+	$hello = "Hello Everyone";
+	$b = "test";
+
+	$testing = array('a','b');
+
+	foreach ($testing as $seat) {
+		# code...
+		echo $$seat . "<br />";
+	}
+	echo $ a ."<br />";
+	
+	i don't have this in php 7
+	*/
+?>
+</body>
+</html>>
