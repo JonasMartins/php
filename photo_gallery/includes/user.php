f4991c2d49b8956fe9ccc589fa56240362485945
JonasMartins <jonas.s.martins@hotmail.com>
2016-09-19 12:26:51 -0300

    Instanciando Objetos User, Video 6-6

--- photo_gallery/includes/user.php
+++ photo_gallery/includes/user.php
@@ -4,7 +4,14 @@
 require_once('database.php');
 
 class User {
-	
+
+	// like database fields
+  public $id;
+  public $username;
+  public $password;
+  public $first_name;
+  public $last_name;
+
   // Método estatico/instância
 	public static function find_all() {
 		return self::find_by_sql("SELECT * FROM users");
@@ -13,6 +20,9 @@ class User {
   public static function find_by_id($id=0) {
     global $database;
     $result_set = self::find_by_sql("SELECT * FROM users WHERE id={$id} LIMIT 1");
+    
+    // Notar que fatch_array na verdade é um simples 
+    // array não um objeto
     $found = $database->fetch_array($result_set);
     return $found;
   }
@@ -24,6 +34,50 @@ class User {
 		return $result_set;
   }
 
+  public function full_name(){
+    if(isset($this->first_name) && isset($this->last_name)){
+      return $this->first_name . " " . $this->last_name;
+    }else { return " "; }
+  }
+
+  // Forma de como o Objeto se construir, sem ter que ficar 
+  // ordenando os valores do arrey e passando para os 
+  // atributos do objeto
+  private static function instantiate($record){
+    // checar antes se record existe e se é de fato um array
+
+    // forma mais dinâmica de se preencher os atributos do 
+    // objeto com as campos do array
+    foreach($record as $attribute=>$value){
+      if($object->has_attribute($attribute)){
+        $object->$attribute = $value;
+      }
+    }
+   /* 
+    // forma mais trabalhosa de se preencher o objeto...
+    // tendo que escrever todos os dados um por um. 
+
+    $object = new self; // User();
+    $object->id         = $record['id'];
+    $object->username   = $record['username'];
+    $object->password   = $record['password'];
+    $object->first_name = $record['first_name'];
+    $object->last_name  = $record['last_name'];
+    */
+    return $object;
+  }
+
+  private function has_attribute($attribute){
+    // get_object_vars retorna um array associado com todos
+    // os atributos, incuindo os privados como keys e seus respectivos
+    // valores como values
+    $object_vars = get_object_vars($this);
+    // Não á importancia com relação aos valores, apenas
+    // precisamos saber se eles de fato existem ou não, retorna true or false
+    return array_key_exists($attribute, $object_vars);
+
+  }
+ 
 }
 
 ?>
--- photo_gallery/public/index.php
+++ photo_gallery/public/index.php
@@ -2,6 +2,9 @@
 require_once("../includes/database.php");
 require_once("../includes/user.php");
 
+/*
+			--- Exemplo antigo.....
+
 // teste simples de conexão
 if(isset($database)) { echo "true"; } else { echo "false"; }
 echo "<br />";
@@ -10,13 +13,13 @@ echo "<br />";
 // objeto estatico vindo da classe database
 echo $database->escape_value("It's working?<br />");
 
-//**** Exemplo de consulta de um banco simples
+//---- Exemplo de consulta de um banco simples
 
-/*
+
  $sql  = "INSERT INTO users (id, username, password, first_name, last_name) ";
  $sql .= "VALUES (1, 'kskoglund', 'secretpwd', 'Kevin', 'Skoglund')";
  $result = $database->query($sql);
-*/
+
 
 $sql = "SELECT * FROM users WHERE id = 1";
 $result_set = $database->query($sql);
@@ -25,6 +28,7 @@ echo $found_user['username'];
 
 echo "<hr />";
 
+*/
 
 /*
 	uma variável que recebe a saída de um método
@@ -37,16 +41,28 @@ echo "<hr />";
 	eis as suas diferenças.
 
 */
+// record pois isto é um array...
+$record = User::find_by_id(1);
+//echo $record['username'];
 
-$found_user = User::find_by_id(1);
-echo $found_user['username'];
+
+
+
+// isso não é um array, mas um atributo de um objeto
+// before fullname: echo $user->username;
+// after full name:
+echo $user->full_name();
 
 echo "<hr />";
 
+
+
+
+/*
 $user_set = User::find_all();
 while ($user = $database->fetch_array($user_set)) {
   echo "User: ". $user['username'] ."<br />";
   echo "Name: ". $user['first_name'] . " " . $user['last_name'] ."<br /><br />";
-}
+}*/
 
 ?>
