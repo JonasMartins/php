caa38a0233b700947aeb75a1b4e8d78d83cba586
JonasMartins <jonas.s.martins@hotmail.com>
2016-09-20 22:46:56 -0300

    Instalando mysql e setando config tabela User

--- photo_gallery/includes/config.php
+++ photo_gallery/includes/config.php
@@ -3,9 +3,14 @@
 // Database Constants
 // Obviamente tudo deve ser mudado depois de acordo com 
 // as minhas configurações
+// problemas com o password? 
+// pilitica média de segurança?
+
+// UNINSTALL PLUGIN validate_password;
+
 defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? null : define("DB_USER", "gallery");
-defined('DB_PASS')   ? null : define("DB_PASS", "phpOTL123");
+defined('DB_USER')   ? null : define("DB_USER", "jonas");
+defined('DB_PASS')   ? null : define("DB_PASS", "123456");   
 defined('DB_NAME')   ? null : define("DB_NAME", "photo_gallery");
 
 ?>
