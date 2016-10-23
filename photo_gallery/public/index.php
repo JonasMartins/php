a9b3478ea2664d651f3335bfd8f945256c651588
JonasMartins <jonas.s.martins@hotmail.com>
2016-10-23 19:41:53 -0300

    Index Layout v2

--- photo_gallery/public/index.php
+++ photo_gallery/public/index.php
@@ -7,89 +7,48 @@
 <!-- 
 	Forma antiga
 <a class="btn btn-primary" href="./admin/index.php" role="button">Admin Login</a>
+
+
+Aqui será interessante usar o descendent
+para evitar repetição de código
 -->
 <body>
-
 <div class="container">
   <div class="row">	
-  	<section class="col-lg-12">
-	  	<form style="float: right" action="./admin/index.php">
+  	<section >
+			<h2 >Create an Account</h2>
+			<div class="form-group">
+			  <label for="usr">User Name:</label>
+			  <input style="width: 300px;" type="text" class="form-control" id="usr">
+			</div>
+			<div class="form-group">
+			  <label for="usr">First Name:</label>
+			  <input style="width: 300px;" type="text" class="form-control" id="usr">
+			</div>
+			<div class="form-group">
+			  <label for="usr">Last Name:</label>
+			  <input style="width: 300px;" type="text" class="form-control" id="usr">
+			</div>
+			<div class="form-group">
+			  <label for="pwd">Password:</label>
+			  <input style="width: 300px;" type="password" class="form-control" id="pwd">
+			</div>
+			<form style="float: right" action="./admin/index.php">
 	    	<input type="submit" class="btn btn-primary btn-md sharp" value="Admin Login" />
 			</form>
+			<form style="float: left" action="./admin/index.php">
+    		<input style="width: 300px;" type="submit" class="btn btn-primary btn-md sharp" value=" Create " />
+			</form>
 		</section>
-	</div>
-  <div class="row">
-    <section class="col-md-6">
-    
-    <!--
-     Adicionando styles a atbelas bootstrap
-    hover dá um highlight na tupla
-
-    table-condensed: deixa mais espremida as colunas
-    table responsive põe um scroll nas tuplas ao 
-    serem espremidas  
-    -->
-      <div class="table-responsive">
-        <table class="table table-striped table-bordered table-hover">
-          <h2>Create an Account</h2>
-          <!--
-          Espécie de cabeçalho da tabela
-          <thead>
-            <tr>
-              <th id="light_tan_th">Create an Account</th>
-              
-            </tr>
-          </thead> -->
-          <tbody>
-            <tr class="active">
-              <th id="light_tan_th" scope="row">User Name</th>
-              <td>Uma espécie de text field</td>
-              <!--
-              <td>Default</td>
-              <td>Default color to a particular row or cell</td> -->
-            </tr>
-            <tr class="active">
-              <th id="light_tan_th" scope="row">First Name</th>
-              <td>Uma espécie de text field</td>
-              <!--
-              <td>Gray</td>
-              <td>Active color to a particular row or cell</td> -->
-            </tr>
-            <tr class="active">
-              <th id="light_tan_th" scope="row">Last Name</th>
-              <td>Uma espécie de text field</td>
-              <!-- 
-              <td>Green</td>
-              <td>A successful or positive action</td>
-            	-->
-            </tr>
-            <tr class="active">
-              <th id="light_tan_th" scope="row">Password</th>
-              <td>Uma espécie de text field</td>
-              <!--
-              <td>Blue</td>
-              <td>A neutral informative change or action</td>
-            	-->
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </section>
-  </div><!-- row -->  
-  <div class="row">	
-  	<section class="col-lg-12 col-md-10">
-  		<form style="float: left" action="./admin/index.php">
-    		<input type="submit" class="btn btn-primary btn-md sharp" value=" Create " />
-			</form> 
-		</section>
-	</div><!-- row --> 
-</div><!-- content container -->
-
+	</div> 
+</div>
 
 <script src="js/jquery-2.1.4.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/script.js"></script>
 </body>
+
+<?php include_layout_template('footer.php'); ?>
 
 
 <!--
@@ -104,6 +63,72 @@ Parte anterior que ficava listando qulquer foto
     <p><?php echo $photo->caption; ?></p>
   </div>
 <?php endforeach;?>
+
+
+
+
+
+Antigo logon dentro de uma tabela
+
+<div class="container">
+  <div class="row">	
+  	<section class="col-lg-12">
+	  	<form style="float: right" action="./admin/index.php">
+	    	<input type="submit" class="btn btn-primary btn-md sharp" value="Admin Login" />
+			</form>
+		</section>
+	</div>
+  <div class="row">
+    <section class="col-md-6">
+    
+     Adicionando styles a atbelas bootstrap
+    hover dá um highlight na tupla
+
+    table-condensed: deixa mais espremida as colunas
+    table responsive põe um scroll nas tuplas ao 
+    serem espremidas  
+      <div class="table-responsive">
+        <table class="table table-striped table-bordered table-hover">
+          <h2>Create an Account</h2>
+          <tbody>
+            <tr class="active">
+              <th id="light_tan_th" scope="row">User Name</th>
+              <td>
+              <input type="text" name="user_name"/>
+              </td>
+            </tr>
+            <tr class="active">
+              <th id="light_tan_th" scope="row">First Name</th>
+              <td>
+              <input type="text" name="user_name" />
+              </td>
+            </tr>
+            <tr class="active">
+              <th id="light_tan_th" scope="row">Last Name</th>
+              <td>
+              <input type="text" name="user_name">
+              </td>
+             </tr>
+            <tr class="active">
+              <th id="light_tan_th" scope="row">Password</th>
+              <td>
+              <input type="text" name="user_name">
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div> 
+  <div class="row">	
+  	<section class="col-lg-12 col-md-10">
+  		<form style="float: left" action="./admin/index.php">
+    		<input type="submit" class="btn btn-primary btn-md sharp" value=" Create " />
+			</form> 
+		</section>
+	</div> 
+</div>
+
+
 -->
 
-<?php include_layout_template('footer.php'); ?>
