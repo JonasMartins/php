f0c14526f4ae41d3d84466d2029fcb396962ceb0
JonasMartins <jonas.w.martins@gmail.com>
2018-07-22 16:15:20 -0300

    lcvjsfs 011

---
+++ laravel/blog/resources/views/admin/articles/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<page size="10">
+  <panel title="Articles">
+  
+  </panel>
+</page>
+@endsection
--- laravel/blog/resources/views/home.blade.php
+++ laravel/blog/resources/views/home.blade.php
@@ -6,7 +6,7 @@
   Test
   <div class="row">
     <div class="col-md-4">
-      <box amount="166" title="Articles" url="#" color="red" icon="ion ion-pie-graph"></box>
+      <box amount="166" title="Articles" url="{{route('articles.index')}}" color="red" icon="ion ion-pie-graph"></box>
     </div>
     <div class="col-md-4">
       <box amount="50" title="Authors" url="#" color="green" icon="ion ion-person"></box>
