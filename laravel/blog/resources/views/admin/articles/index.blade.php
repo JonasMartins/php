c096e22fc96c27ec676a431a782a79283ff35446
JonasMartins <jonas.w.martins@gmail.com>
2018-07-22 16:22:47 -0300

    lcvjsfs 012

--- laravel/blog/resources/views/admin/articles/index.blade.php
+++ laravel/blog/resources/views/admin/articles/index.blade.php
@@ -3,7 +3,35 @@
 @section('content')
 <page size="10">
   <panel title="Articles">
-  
+    <a href="#">Create</a>
+    <table class="table table-striped table-hover">
+      <thead>
+        <tr>
+      
+          <th>#</th>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Author</th>
+          <th>Date</th>
+          <th>!</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>
+            <a href="#">Edit</a> |
+            <a href="#">Delete</a>
+          </td>
+        </tr>
+        
+      </tbody>
+    </table>
   </panel>
 </page>
 @endsection
