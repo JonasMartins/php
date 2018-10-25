19aaeaedaf630e3ef5f7703287d5a03ef3d2f3fc
JonasMartins <jonas.w.martins@gmail.com>
2018-10-24 21:23:23 -0300

    fwdwlv 194

--- laravel/fwdwlv/app/User.php
+++ laravel/fwdwlv/app/User.php
@@ -34,11 +34,5 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
-    public function setTitleAttribute($value)
-    {
-        $this->attributes['title'] = $value;
-        $this->attributes['slug'] = str_slug($value);    
-    }
-
 
 }
