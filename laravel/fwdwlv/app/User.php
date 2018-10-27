399f47afba68d8f0427939eaa8c872d153b0a479
JonasMartins <jonas.w.martins@gmail.com>
2018-10-26 23:07:10 -0300

    fwdwlv 192

--- laravel/fwdwlv/app/User.php
+++ laravel/fwdwlv/app/User.php
@@ -34,5 +34,9 @@ class User extends Authenticatable
         return $this->hasMany(Question::class);
     }
 
-
+    public function getUrlAttribute()
+    {
+        //return route("question.show", $this->id);
+        return '#';
+    }
 }
